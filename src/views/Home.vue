6256aefa1cb0afe0484e9c34f168c7b0815ab872
Adam <adam7tt@gmail.com>
2019-10-20 15:04:33 -0700

    first commit

---
+++ src/views/Home.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="home container">
+    <div class="card" v-for="smoothie in smoothies" :key="smoothie.id">
+      <div class="card-content">
+        <i class="material-icons delete" @click="deleteSmoothie(smoothie.id)">delete</i>
+        <h2 class="indigo-text">
+          {{ smoothie.title }}
+        </h2>
+          <ul class="ingredients">
+            <li v-for="(ingredient, index) in smoothie.ingredients" :key="index">
+              <span class="chip">
+                {{ ingredient }}
+              </span>
+            </li>
+          </ul>
+      </div>
+      <span class="btn-floating btn-small halfway-fab pink">
+        <router-link :to="{name: 'EditSmoothie', params: {smoothie_slug: smoothie.slug} }">
+          <i class="material-icons eidt">edit</i>
+        </router-link>
+      </span>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import firestore from '@/firebase/init'
+
+export default {
+  name: 'home',
+  data(){
+    return{
+      smoothies:[
+      ],
+    }
+  },
+  methods: {
+    deleteSmoothie(id){
+      firestore.collection('smoothies').doc(id).delete()
+      .then(() =>{
+        this.smoothies = this.smoothies.filter(smoothie => {
+          return smoothie.id != id
+        })
+      })
+    }
+  },
+  created(){
+    firestore.collection('smoothies').get()
+    .then(snapshot => {
+      snapshot.forEach(doc => {
+        let smoothie = doc.data()
+        smoothie.id = doc.id
+        this.smoothies.push(smoothie)
+      })
+    })
+  }
+}
+
+</script>
+
+<style>
+  .home{
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-gap: 30px;
+    margin-top: 60px;
+  }
+
+   .home h2{
+     font-size: 1.8em;
+     text-align: center;
+     margin-top: 0;
+   }
+   .home .ingredients{
+     margin: 30px auto;
+   }
+   
+   .home .ingredients li{
+     display: inline-block;
+   }
+  .home .delete{
+    position: absolute;
+    top: 4px;
+    right: 4px;
+    cursor: pointer;
+    color: #aaa;
+    font-size: 1.4ex;
+  }
+</style>
